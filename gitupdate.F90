e5bd15ec4b0063a5b39d71c409d4fe191db8c699
Git use for Nora <lumingze999@gmail.com>
2023-02-04 11:07:59 +0800

    F90 update2

--- gitupdate.F90
+++ gitupdate.F90
@@ -4,7 +4,7 @@
 !# File Name: gitupdate.F90
 !# Created on : 2023-02-04 10:58:26
 !# Author: YunhaoFu
-!# Last Modified: 2023-02-04 10:58:57
+!# Last Modified: 2023-02-04 11:07:40
 !# Description: works for fortran90 with pre-compile
 !#########################################################################
 
@@ -13,3 +13,7 @@ PROGRAM main
     PRINT *, "Update over"
 
 ENDPROGRAM main
+
+SUBROUTINE test
+
+ENDSUBROUTINE test
